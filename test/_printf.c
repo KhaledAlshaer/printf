4fb38237d5ec66cdcfb39605db288f81e6706070
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-09-19 12:16:36 -0700

    task 0 yarb

--- test/_printf.c
+++ test/_printf.c
@@ -8,29 +8,58 @@
 
 int _printf(const char *format, ...)
 {
-	int printingCounter, printedLength = 0;
+	int i, byte_count = 0;
 	va_list args;
 
 	va_start(args, format);
 
-	isFormatValid(format);
-
-	while (*format)
+	while (format[i] != '\0')
 	{
-		if (*format == '%')
+		if (format[i] == '%')
 		{
-			format++;
-
-			printingCounter = printf_helper(*format, args);
-
+			i++;
+			if (format[i] == 'c')
+			{
+			}
+			else if (format[i] == 's')
+			{
+			}
+			else if (format[i] == 'd')
+			{
+			}
+			else if (format[i] == 'i')
+			{
+			}
+			else if (format[i] == '%')
+			{
+			}
 		}
-		else
+		if (format[i] == '\\')
 		{
-			printingCounter = _putchar(*format);
-		}
+			ssize_t out;
 
-		printedLength += printingCounter;
-		format++;
+			i++;
+			if (format[i] == 'n')
+			{
+				out = write(1, "\n", 1);
+			}
+			else if (format[i] == 't')
+			{
+				out = write(1, "\t", 1);
+
+			}
+			else if (format[i] == '\\')
+			{
+				out = write(1, "\\", 1);
+			}
+
+			if (out == -1)
+			{
+				perror("_printf");
+				va_end(args);
+				return (-1);
+			}
+		}
 	}
 
 	va_end(args);
--- test/main.h
+++ test/main.h
@@ -1,21 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 /**
  * struct specifiers_struct - struct for specifiers
- * @specify: format specifier
+ * @flag: format specifier
  * @f: format function
 */
 
 typedef struct specifiers_struct
 {
-	char specify;
+	char *flag;
 	int (*f)(va_list);
 } specifiers;
 
@@ -29,6 +28,7 @@ int isFormatValid(const char *format);
 int handle_c(va_list args);
 int handle_s(va_list args);
 int handle_percent(va_list args);
+int handle_specials(va_list args);
 
 int handle_di(va_list args);
 
