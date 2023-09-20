96fda17c235335dda33de54225d990b94196a40d
samehelgazar95 <selgazar95@gmail.com>
2023-09-19 19:48:24 -0700

    Last isa

--- _printf.c
+++ _printf.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * slashChars- slach handlers
+ * @c: char
+ * Return: Nothing
+*/
+
+void slashChars(char c)
+{
+	if (c == 'n')
+		write(1, "\n", 1);
+	else if (c == 'r')
+		write(1, "\r", 1);
+	else if (c == 'a')
+		write(1, "\a", 1);
+	else if (c == 'b')
+		write(1, "\b", 1);
+	else if (c == 't')
+		write(1, "\t", 1);
+	else if (c == 'f')
+		write(1, "\f", 1);
+}
+
+/**
  * handle_bs - handles something
  * @c: string to handle
  * Return: return length of it
@@ -10,15 +32,7 @@ int handle_bs(char c)
 {
 	int charPrinted;
 
-	if (c == 'n')
-	{
-		charPrinted = write(1, "\n", 1);
-	}
-	else if (c == 't')
-	{
-		charPrinted = write(1, "\t", 1);
-	}
-	else if (c == '0')
+	if (c == '0')
 	{
 		write(1, "\0", 1);
 	}
@@ -34,10 +48,8 @@ int handle_bs(char c)
 	{
 		charPrinted = write(1, "\"", 1);
 	}
-	else if ('r')
-	{
-		write(1, "\r", 1);
-	}
+
+	slashChars(c);
 
 	return (charPrinted);
 }
